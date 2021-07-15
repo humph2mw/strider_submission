561d128792d8586f924d710b5cca74fde6773b96
Matthew Humphreys <matthewhumphreys@Matthews-MacBook-Air.local>
2021-07-15 17:34:17 -0400

    updated tests

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -9,19 +9,31 @@ import {
 import data from '../pages/dashboard/receipts.json'
 
 describe('test utility functions', () => {
-  test('should calculate the correct number of recurring customers', () => {
-    expect(calculateAverageValueOfOrders(data)).toBe(32)
+  describe('test calculateAverageValueOfOrders', () => {
+    test('should calculate the correct number of recurring customers', () => {
+      expect(calculateAverageValueOfOrders(data)).toBe(32)
+    })
   })
 
-  test('should calculate the average number of items per order', () => {
-    expect(calculateAverageNumberOfItemsPerOrder(data)).toBe(2.375)
+  describe('test calculateAverageNumberOfItemsPerOrder', () => {
+    test('should calculate the average number of items per order', () => {
+      expect(calculateAverageNumberOfItemsPerOrder(data)).toBe(2.375)
+    })
   })
 
-  test('should calculate the number of recurring customers', () => {
-    expect(calculateNumberOfRecurringCustomers(data)).toBe(2)
+  describe('test calculateNumberOfRecurringCustomers', () => {
+    test('should calculate the number of recurring customers', () => {
+      expect(calculateNumberOfRecurringCustomers(data)).toBe(2)
+    })
   })
 
-  test('should get the correct number of date occurences', () => {
-    expect(getDateOccurence(data, '2021-02-01')).toBe(1)
+  describe('test getDateOccurence', () => {
+    test('should get the correct number of date occurences', () => {
+      expect(getDateOccurence(data, '2021-02-01')).toBe(1)
+    })
+
+    test('should not find any occurences for a date not in the array', () => {
+      expect(getDateOccurence(data, '2021-03-01')).toBe(0)
+    })
   })
 })
