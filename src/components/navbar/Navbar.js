63105e1f2822b02b4a57ee8ab09a210851393e8a
Matthew Humphreys <matthewhumphreys@Matthews-MacBook-Air.local>
2021-07-15 07:03:33 -0400

    initial setup of a navbar and dashboard page

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import './style.css'
+
+export default function Nav() {
+  return (
+    <div>
+      <Navbar id="navbar">
+        <Navbar.Brand>Strider General Store</Navbar.Brand>
+      </Navbar>
+    </div>
+  )
+}
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './style.css';
+
+export default function Dashboard() {
+  return (
+    <Container id="dashboardContainer">
+      Dash
+    </Container>
+  )
+}
